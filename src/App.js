70325c76d4e5ab26b395f1949836556131967a31
Mohammed Abdul Rasheed <rashid0241@gmail.com>
2022-04-16 12:25:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 
 
-const API_URL ='http://www.omdbapi.com?apikey=7bfb0834';
+const API_URL ='https://www.omdbapi.com?apikey=7bfb0834';
 const App=()=> {
 
     const [searchTerm, setSearchTerm] = useState("");
