413993172c72ea11996374af132caa35b39418d1
Mohammed Abdul Rasheed <rashid0241@gmail.com>
2022-04-17 09:12:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App=()=> {
         <h1> Movie World</h1>
 
             <div className="search">
-                <input placeholder ="Adnan Ali , BahuBali" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+                <input placeholder ="Shah Zaid is A Fucking Asshole Pervert Bulldog" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
                 <img
                     src={SearchIcon}
                     alt="search"
